2f842373033058ae47336e60a47743ccc474b358
Megan Gonzales <megan.campbell26@yahoo.com>
2021-06-02 15:14:43 -0600

    Add relationships, validations and test for them

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationship' do
+    it { should have_many :customer_subscriptions }
+    it { should have_many(:customers).through(:customer_subscriptions) }
+    it { should have_many :subscription_teas }
+    it { should have_many(:teas).through(:subscription_teas) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+  end
 end
