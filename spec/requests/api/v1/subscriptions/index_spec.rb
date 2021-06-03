f22d89e3ff7645ceb75f284aee08175b180bb1ea
Megan Gonzales <megan.campbell26@yahoo.com>
2021-06-03 08:18:10 -0600

    add subscriptions index and refactor test

--- spec/requests/api/v1/subscriptions/index_spec.rb
+++ spec/requests/api/v1/subscriptions/index_spec.rb
@@ -13,10 +13,31 @@ RSpec.describe "customer's subscriptions index" do
       expect(response.status).to eq(200)
 
       result = JSON.parse(response.body, symbolize_names: true)
-
+      
       expect(result).to be_a(Hash)
       expect(result).to have_key(:data)
-      expect(result[:data]).to be_a(Hash)
+      expect(result[:data]).to be_an(Array)
+      expect(result[:data].count).to eq(2)
+
+      result[:data].each do |subscription|
+        expect(subscription).to be_a(Hash)
+        expect(subscription).to have_key(:id)
+        expect(subscription[:id]).to be_a(String)
+        expect(subscription).to have_key(:type)
+        expect(subscription[:type]).to be_a(String)
+        expect(subscription[:type]).to eq('subscription')
+        expect(subscription).to have_key(:attributes)
+        expect(subscription[:attributes]).to be_a(Hash)
+        expect(subscription[:attributes]).to have_key(:title)
+        expect(subscription[:attributes][:title]).to be_a(String)
+        expect(subscription[:attributes]).to have_key(:price)
+        expect(subscription[:attributes][:price]).to be_a(Float)
+        expect(subscription[:attributes]).to have_key(:status)
+        expect(subscription[:attributes][:status]).to be_a(String)
+        expect(subscription[:attributes]).to have_key(:frequency)
+        expect(subscription[:attributes][:frequency]).to be_a(String)
+      end
+
     end
   end
 end
