d4ce6744bfc349f1c361911b76a02977b07500a2
Megan Gonzales <megan.campbell26@yahoo.com>
2021-06-05 14:22:21 -0600

    Add conditional for destroy action

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -17,6 +17,11 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   def destroy
     customers_subscription = CustomerSubscription.where(subscription_id: params[:subscription_id])
-    CustomerSubscription.destroy(customers_subscription.ids)
+    if customers_subscription == []
+      error = "No such subscription exists"
+      render json: ErrorSerializer.new(error), status: :bad_request
+    else
+      CustomerSubscription.destroy(customers_subscription.ids)
+    end
   end
 end
